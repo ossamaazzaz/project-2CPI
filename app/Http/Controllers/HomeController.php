35b9b88096d24b1c9e40acbb28b74092fbbe1388
Oussama Messabih~TheHappyBit <o.messabih@esi-sba.dz>
2018-03-09 16:18:30 +0100

    resolve merging problem

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,34 +25,4 @@ class HomeController extends Controller
     {
         return view('home');
     }
-    public function edit()
-    {
-        return view('auth.edit');
-    }
-    public function update(Request $req) {
-        $user = \Auth::user();
-        $validator =  \Validator::make($req->all(), [
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-        $emailCheck = ($req->input('email') != '') && ($req->input('email') != $user->email);
-         $rules = [];
-        if ($emailCheck) {
-                $rules = [
-                'email' => 'email|max:255|unique:users',
-            ];
-             $validator = $this->validator($req->all(), $rules);  
-        }
-        
-
-        if ($validator->fails()) {
-            return back()->withErrors($validator)->withInput();
-        }
-        if($emailCheck) {
-            $user->email = $req->all()['email'];   
-        }
-        
-        $user->password = \Hash::make($req->all()['password']);
-        $user->save();
-        return view('auth.edit');
-    }
 }
