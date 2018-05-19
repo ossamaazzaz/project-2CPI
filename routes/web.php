fb45112f7cc2028677c829a6449bb894795cf9e5
cra3ker <m.iferroudjene@esi-sba.dz>
2018-05-19 06:38:43 +0200

    implementation off EditProfil page

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/contact','ContactUsController@index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home/edit','HomeController@edit');
 Route::post('/home/edit','HomeController@update');
+
 Route::get('/home/index2','HomeController@index2');
 //Comments
 Route::post('/home/{product}/comments','CommentsController@addComment');
